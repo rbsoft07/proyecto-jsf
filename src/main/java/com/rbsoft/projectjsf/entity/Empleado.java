abdf21fb8d72b47b380a3d7d63150553af75b1e7
rbsoft07 <48659149+rbsoft07@users.noreply.github.com>
2022-08-22 12:48:09 -0400

    SECCION6_JSF_VIDEO46 -> Manejo de datatable y creacion de la entidad del datatable.

---
+++ src/main/java/com/rbsoft/projectjsf/entity/Empleado.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package com.rbsoft.projectjsf.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author RBSOFT Clase que representa a un empleado
+ */
+public class Empleado implements Serializable {
+
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
+	private String puesto;
+	private boolean estatus;
+
+	public Empleado() {
+
+	}
+
+	public Empleado(String nombre, String primerApellido, String segundoApellido, String puesto, boolean estatus) {
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+		this.puesto = puesto;
+		this.estatus = estatus;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the primerApellido
+	 */
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	/**
+	 * @param primerApellido the primerApellido to set
+	 */
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	/**
+	 * @return the segundoApellido
+	 */
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	/**
+	 * @param segundoApellido the segundoApellido to set
+	 */
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	/**
+	 * @return the puesto
+	 */
+	public String getPuesto() {
+		return puesto;
+	}
+
+	/**
+	 * @param puesto the puesto to set
+	 */
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	/**
+	 * @return the estatus
+	 */
+	public boolean isEstatus() {
+		return estatus;
+	}
+
+	/**
+	 * @param estatus the estatus to set
+	 */
+	public void setEstatus(boolean estatus) {
+		this.estatus = estatus;
+	}
+
+}

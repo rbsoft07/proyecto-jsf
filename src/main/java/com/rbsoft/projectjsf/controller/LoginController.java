a5886159901a5878cc1c3c2a134c3980019ad16e
rbsoft07 <48659149+rbsoft07@users.noreply.github.com>
2022-08-22 11:43:35 -0400

    SECCION6_JSF_VIDEO43
    Manejo de redireccion de paginas, y se agrega pantalla principal al
    acceder.

--- src/main/java/com/rbsoft/projectjsf/controller/LoginController.java
+++ src/main/java/com/rbsoft/projectjsf/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.rbsoft.projectjsf.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.FaceletContext;
 
@@ -31,8 +33,16 @@ public class LoginController implements Serializable {
 		System.out.println(" Usuario : " + usuario);
 
 		if (usuario.equals("user") && clave.equals("1234")) {
-			FacesContext.getCurrentInstance().addMessage("idFormLogin:txtIdUsuario",
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario y/o Clave Correcto", ""));
+//			FacesContext.getCurrentInstance().addMessage("idFormLogin:txtIdUsuario",
+//					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario y/o Clave Correcto", ""));
+			
+			try {
+				this.redireccionar("principal.xhtml");
+			} catch (IOException e) {
+				FacesContext.getCurrentInstance().addMessage("idFormLogin:txtIdUsuario",
+						new FacesMessage(FacesMessage.SEVERITY_FATAL, "Pagina no encontrada!!", ""));
+				e.printStackTrace();
+			}
 		} else {
 			FacesContext.getCurrentInstance().addMessage("idFormLogin:txtIdUsuario",
 					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario y/o Clave incorrecta", ""));
@@ -40,6 +50,12 @@ public class LoginController implements Serializable {
 
 	}
 
+	
+	private void redireccionar(String pagina) throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(pagina);
+	}
+	
 	/**
 	 * @return the usuario
 	 */

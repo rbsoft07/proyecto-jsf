7288f42fafeada80cb2f31d23dfc69c28da7421e
RCBA_91 <rbsoft07@gmail.com>
2025-07-03 16:09:28 -0400

    Configuracion applicationConttext para integracion con spring

--- src/test/java/com/rbsoft/projectjsf/spring/test/SpringProjectJSF.java
+++ src/test/java/com/rbsoft/projectjsf/spring/test/SpringProjectJSF.java
@@ -10,17 +10,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.rbsoft.projectjsf.dao.DisqueraDAO;
+
 /**
  * @author rbsoft07
  * Clase de prueba unitaria que permite realizar pruebas utilizado el framework Spring
+ * @param <DiqueraDAO>
  */
 public class SpringProjectJSF {
 	
 	@Test
 	void testContext() {
+		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("src/main/resources/applicationContext.xml");
-		assertNotNull(getClass());
+		assertNotNull(context);
+		
+		DisqueraDAO disqueraDao = (DisqueraDAO) context.getBean("disqueraDAO");
+		assertNotNull(disqueraDao);
+		
 		System.out.println("Contexto cargado Correctamente!. ");
+		System.out.println(disqueraDao.toString());
 	}
 
 }

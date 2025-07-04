b7bbfa3ca1e03a4afa73b52b5279f29bec7aeb7f
RCBA_91 <rbsoft07@gmail.com>
2025-07-04 16:40:06 -0400

    Se agrega configuraicon del archivo properties, se renombra el archivo applicationContext.xml Se agregan test de la creacion de objetos por medio de spring.

--- src/test/java/com/rbsoft/projectjsf/spring/test/SpringProjectJSFTest.java
+++ src/test/java/com/rbsoft/projectjsf/spring/test/SpringProjectJSFTest.java
@@ -5,6 +5,7 @@ package com.rbsoft.projectjsf.spring.test;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -17,20 +18,28 @@ import com.rbsoft.projectjsf.dao.DisqueraDAO;
  * Clase de prueba unitaria que permite realizar pruebas utilizado el framework Spring
  * @param <DiqueraDAO>
  */
-public class SpringProjectJSF {
+public class SpringProjectJSFTest {
 	
 	@Test
 	void testContext() {
 		
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("src/main/resources/applicationContext.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
 		assertNotNull(context);
 		
 		DisqueraDAO disqueraDao = (DisqueraDAO) context.getBean("disqueraDAO");
 		assertNotNull(disqueraDao);
 		
+		DisqueraDAO disqueraDaoSecond = (DisqueraDAO) context.getBean("disqueraDAO");
+		
 		System.out.println("Contexto cargado Correctamente!. ");
-		System.out.println(disqueraDao.toString());
+		System.out.println(disqueraDao);
+		System.out.println(disqueraDaoSecond);
+		
+		//::::::::::PROPERTIES:::::::::::::::
+		
+	 	Properties properties =  (Properties) context.getBean("properties");
+	 	System.out.println(properties.get("spring-username"));
 	}
 
 }
